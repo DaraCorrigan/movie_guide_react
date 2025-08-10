fb72583bb0daa40ce3aa126176845d0fe8c8718d
DaraCorrigan <148446121+DaraCorrigan@users.noreply.github.com>
2025-08-10 17:25:53 +0100

    Added Favourites page with an empty state

---
+++ frontend/src/pages/Favourites.jsx
@@ -0,0 +1,8 @@
+function Favourites() {
+    return <div className="favourites-empty">
+        <h2>No favourites added yet</h2>
+        <p>When you add a movie to your favourites they will show here</p>
+    </div>
+}
+
+export default Favourites
