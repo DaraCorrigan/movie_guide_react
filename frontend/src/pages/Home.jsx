a4eadd133e3a7b484f02f1c03faef5b341079b7c
DaraCorrigan <148446121+DaraCorrigan@users.noreply.github.com>
2025-08-10 16:37:14 +0100

    Add search form to Home page
    - Added search UI to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,14 +2,30 @@ import MovieCard from "../components/MovieCard"
 
 function Home() {
     const movies = [
-        {id: 1, title: "John Wick", release_date: "2020"},
-        {id: 2, title: "Terminator", release_date: "1999"},
-        {id: 3, title: "The Matrix", release_date: "1998"},
-    ]
+        { id: 1, title: "John Wick", release_date: "2020" },
+        { id: 2, title: "Terminator", release_date: "1999" },
+        { id: 3, title: "The Matrix", release_date: "1998" },
+    ];
 
-    return <div className="home">
-        <div className="movies-grid">
-            {movies.map(movie => <MovieCard movie={movie} key={movie.id} />)}
+    const handleSearch = () => {
+
+    }
+
+    return ( 
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input type="text" placeholder="Search for a movie..." className="search-input" />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+            
+
+            <div className="movies-grid">
+                {movies.map(movie => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
         </div>
-    </div>
+    );
 }
+
+export default Home
