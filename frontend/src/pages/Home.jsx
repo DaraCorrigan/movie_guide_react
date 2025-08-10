927350ef626c3f83745a91a71e96f5d60b91f3f7
DaraCorrigan <148446121+DaraCorrigan@users.noreply.github.com>
2025-08-10 17:04:54 +0100

    Add search input state and handler to Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,25 @@
 import MovieCard from "../components/MovieCard"
+import {useState} from "react"
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         { id: 1, title: "John Wick", release_date: "2020" },
         { id: 2, title: "Terminator", release_date: "1999" },
         { id: 3, title: "The Matrix", release_date: "1998" },
     ];
 
-    const handleSearch = () => {
-
-    }
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("");
+    };
 
     return ( 
         <div className="home">
             <form onSubmit={handleSearch} className="search-form">
-                <input type="text" placeholder="Search for a movie..." className="search-input" />
+                <input type="text" placeholder="Search for a movie..." className="search-input" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
                 <button type="submit" className="search-button">Search</button>
             </form>
             
